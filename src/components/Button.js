91b9525ac9e529f70c0ec5a6378996e48c57cc35
Swan Htet <57400158+Swanhtet-grc@users.noreply.github.com>
2020-10-15 17:24:43 +0630

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const ButtonContainer = styled.button`
   text-transform: capitalize;
   font-size: 1.4em;
-  background: transparent;
   border: 0.05rem solid var(--lightBlue);
   border-color: ${props =>
     props.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
